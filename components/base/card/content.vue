219d0dcc9e8c7d3e7ce5e1acf9f50fb23382c6c2
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-08 23:24:07 +0700

    create base card

---
+++ components/base/card/content.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="inline-flex flex-col align-baseline items-baseline p-5 text-base">
+    <slot></slot>
+  </div>
+</template>
---
+++ components/base/card/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-col bg-white rounded-md drop-shadow-sm overflow-hidden">
+    <slot></slot>
+  </div>
+</template>
