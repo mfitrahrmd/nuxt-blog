72c84c94222e42eb14f87cd6c64323d689dbf9ac
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-08 23:23:04 +0700

    create base chip

---
+++ components/base/Chip.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class=" inline text-sm px-2 py-1 rounded-md hover:bg-gray-100 border border-transparent hover:border-gray-300 cursor-pointer">
+    <slot></slot>
+  </div>
+</template>
