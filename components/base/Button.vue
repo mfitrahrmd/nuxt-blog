a6d69dad9b50596fd93297f723ee58a581cb31bb
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-08 23:22:35 +0700

    create base button

---
+++ components/base/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    class="px-4 py-1 rounded-sm"
+    :class="{
+        'bg-none border border-primary' : outlined,
+        'bg-none border-none' : text
+    }"
+    v-bind="$attrs"
+  >
+    <slot>
+    </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+    text: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
