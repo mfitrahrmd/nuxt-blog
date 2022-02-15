8905619f45db92dfeec193cb4a3abff167cec3d0
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 01:41:45 +0700

    add base component input

---
+++ components/base/Input.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex flex-col w-full">
+    <label
+      v-if="label"
+      :for="getIdString"
+      class="mb-2 font-medium"
+    >{{ label }}</label>
+    <input
+      :id="getIdString"
+      @input="(e) => $emit('input', e.target.value)"
+      v-bind="$attrs"
+      class="px-2 py-2 box-content rounded-md border outline-none border-gray-300 hover:border-gray-400 focus:border-primary"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  data() {
+    return {
+      id: "",
+    };
+  },
+  props: {
+    label: {
+      type: String,
+    },
+  },
+  computed: {
+    getIdString() {
+      return this.id.toString();
+    },
+  },
+  mounted() {
+    this.id = Math.random();
+  },
+};
+</script>
