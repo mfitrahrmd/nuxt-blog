2abd0d5a5d172beaa5544284fc13cb7d5c3e450c
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 01:42:11 +0700

    add base component list

---
+++ components/base/List.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col space-y-3">
+    <div
+      v-if="title"
+      class="title"
+    >{{ title }}</div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
