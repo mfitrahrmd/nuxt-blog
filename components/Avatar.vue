aada2cd657d2ac7d078cc96f0bdd8c765d56be76
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 01:42:47 +0700

    add component avatar

---
+++ components/Avatar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="rounded-full overflow-hidden"
+    :class="[`w-${size} h-${size}`]"
+  >
+    <img
+      v-bind="$attrs"
+      class="object-cover"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    size: {
+      type: [Number, String],
+      default: "8",
+    },
+  },
+};
+</script>
