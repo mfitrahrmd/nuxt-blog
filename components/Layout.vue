f1513ef231104ff44b2e8ef215b23c17b68124d8
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 01:46:32 +0700

    add some base components

---
+++ components/Layout.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-[1fr,3fr] lg:grid-cols-[2fr,5fr,3fr] gap-4 max-w-site-width p-4 m-auto">
+    <slot></slot>
+  </div>
+</template>
---
+++ components/post/Leftbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <aside class="z-50">
+    <div class="flex fixed bottom-0 w-full bg-white md:bg-transparent border md:border-0 py-2 md:py-0 justify-between px-8 md:flex-col items-center md:space-y-4 md:sticky md:top-header-2">
+      <div class="flex items-center group text-center cursor-pointer">
+        <span class="material-icons p-2 rounded-full group-hover:bg-pink-100 group-hover:text-pink-500">favorite_border</span>
+        <span>x</span>
+      </div>
+      <div class="flex items-center group text-center cursor-pointer">
+        <span class="material-icons p-2 rounded-full group-hover:bg-emerald-100 group-hover:text-emerald-500">podcasts</span>
+        <span>x</span>
+      </div>
+      <div class="flex items-center group text-center cursor-pointer">
+        <span class="material-icons p-2 rounded-full group-hover:bg-indigo-100 group-hover:text-indigo-500">bookmark_border</span>
+        <span>x</span>
+      </div>
+      <div>
+      </div>
+      <BaseMenu
+        top
+        right
+        class="whitespace-nowrap"
+      >
+        <template v-slot:activator="{ on }">
+          <div
+            v-on="on"
+            class="group text-center cursor-pointer"
+          >
+            <span class="material-icons p-2 rounded-full group-hover:bg-gray-100 group-hover:text-gray-500">more_horiz</span>
+          </div>
+        </template>
+        <template>
+          <div class="flex flex-col">
+            <BaseButton
+              text
+              link
+              color="primary"
+              class="text-left"
+            >
+              Share to ...
+            </BaseButton>
+            <BaseButton
+              text
+              link
+              color="primary"
+              class="text-left"
+            >
+              Share to ...
+            </BaseButton>
+            <BaseButton
+              text
+              link
+              color="primary"
+              class="text-left"
+            >
+              ...
+            </BaseButton>
+          </div>
+        </template>
+      </BaseMenu>
+
+    </div>
+  </aside>
+</template>
