8bd8c2989e0ac78776a91143f8d3baa508b0fe81
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 01:43:43 +0700

    add component footer

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="w-full bg-gray-200">
+    <div class="min-h-[300px] flex flex-col justify-center">
+      <h4 class="text-center text-gray-700">Blog Community — Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsa blanditiis nostrum consequatur eaque distinctio. <br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, a? <br> Made with love and Nuxt Js. Blog Community.</h4>
+    </div>
+  </footer>
+</template>
