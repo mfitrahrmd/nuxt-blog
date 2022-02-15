eccf749adb9addab0e0145813bb9b79e8c32b98a
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 01:41:57 +0700

    add base component menu

---
+++ components/base/Menu.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="relative">
+    <slot
+      name="activator"
+      :on="{click: () => show = !show}"
+    ></slot>
+    <transition name="fade">
+      <div
+        v-if="show"
+        v-bind="$attrs"
+        class="bg-white min-w-[200px] px-2 py-1 mt-1 border rounded-md shadow-md absolute"
+        :class="[{'bottom-full' : top}, {'top-full' : bottom}, {'right-full' : left}, {'left-full' : right}]"
+      >
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  data() {
+    return {
+      show: false,
+    };
+  },
+  props: {
+    top: {
+      type: Boolean,
+    },
+    left: {
+      type: Boolean,
+    },
+    bottom: {
+      type: Boolean,
+    },
+    right: {
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
