4ea7bf94b01cfd94e44bfa962d36321c592c176d
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 01:41:28 +0700

    add base component button

--- components/base/Button.vue
+++ components/base/Button.vue
@@ -1,11 +1,14 @@
 <template>
   <button
-    class="px-4 py-1 rounded-sm"
-    :class="{
-        'bg-none border border-primary' : outlined,
-        'bg-none border-none' : text
-    }"
-    v-bind="$attrs"
+    class="px-4 py-1 rounded-md"
+    :class="[
+        outlined ? `bg-none border border-${color}` : '',
+        text ? 'bg-none border-none' : '',
+        tile ? 'rounded-none' : '',
+        link ? `px-2 py-2 hover:underline hover:bg-${color}-50 hover:text-${color}` : '',
+        dark ? 'text-white' : '',
+        light ? 'text-black' : ''
+    ]"
   >
     <slot>
     </slot>
@@ -14,13 +17,35 @@
 
 <script>
 export default {
-  inheritAttrs: false,
+  data() {
+    return {};
+  },
   props: {
     outlined: {
       type: Boolean,
       default: false,
     },
     text: {
+      type: Boolean,
+      default: false,
+    },
+    tile: {
+      type: Boolean,
+      default: false,
+    },
+    link: {
+      type: Boolean,
+      default: false,
+    },
+    color: {
+      type: String,
+      default: "primary",
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+    light: {
       type: Boolean,
       default: false,
     },
