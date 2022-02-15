cd5bc67eed03969645e7997c59285088a115b42a
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-02-16 01:45:01 +0700

    edit error layout

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="min-h-body-height w-full flex flex-col justify-center items-center">
+      <div class="display-1">{{ error.statusCode }}</div>
+      <div class="text-lg">{{ error.message }}</div>
+      <nuxt-link
+        to="/"
+        class="underline text-blue-500"
+      >
+        back to homepage
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+};
+</script>
